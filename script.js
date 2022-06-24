25542e2dea982e218ee7cad4fb57578ffc0a8cbf
Alabi Sunday <alabisundayayofe@gmail.com>
2022-06-24 02:50:02 -0700

    First push, intro to js

---
+++ script.js
@@ -0,0 +1,9 @@
+//initializing and assigning values to variables
+let myName = "Alabi Sunday";
+let height = "6ft";
+let country = "Nigeria";
+
+//Printing to the console in a user's friendly way
+console.log("My name is " + myName);
+console.log("I am " + height);
+console.log("I am from " + country);
